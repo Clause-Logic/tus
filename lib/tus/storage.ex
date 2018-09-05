01f24f263f0b303b70be9884c48ec879f973d5af
Gullit Miranda <gullitmiranda@gmail.com>
2018-09-05 17:05:00 -0300

    Adding storage helpers to file_path/2 and url/2 in Tus.Storage

--- lib/tus/storage.ex
+++ lib/tus/storage.ex
@@ -20,4 +20,18 @@ defmodule Tus.Storage do
   def delete(%Tus.File{} = file, %{storage: storage} = config) do
     storage.delete(file, config)
   end
+
+  @doc """
+  Get uid file path.
+  """
+  def file_path(uid, %{storage: storage} = config) do
+    storage.file_path(uid, config)
+  end
+
+  @doc """
+  Get uid file url.
+  """
+  def url(uid, %{storage: storage} = config) do
+    storage.url(uid, config)
+  end
 end
